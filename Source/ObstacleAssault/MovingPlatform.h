12a335c4e592256b56af9512828b49227157179d
Victor Cerezini <victorcerezini@hotmail.com>
2022-07-16 03:46:17 -0300

    Project files added.

---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMovingPlatform();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	
+private:
+
+	UPROPERTY(EditAnywhere, Category="Moving")
+		FVector PlatformVelocity = FVector(100, 0, 0);
+	UPROPERTY(EditAnywhere, Category = "Moving")
+		float MoveDistance = 100;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+		FRotator RotationVelocity;
+
+	FVector StartLocation;
+
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
+
+	bool ShouldPlatformReturn() const;
+	float GetDistanceMoved() const;
+};
